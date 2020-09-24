4b21669cbb069a6a88b0319e734afeefc11ae3a6
StellaKim1230 <stellakim1230@gmail.com>
2020-09-24 17:26:29 +0900

    자물쇠 열쇠 푸는중.

--- study/20200929.js
+++ study/20200929.js
@@ -1,25 +1,51 @@
 // 자물쇠와 열쇠 (완전탐색)
 const key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
 const lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+
 function solution(key, lock) {
   const background = new Array(lock.length + (key.length -1) * 2)
   // 처음 lock 위치
-  background[key.length -1, key.length -1] = lock[0,0][0]
-  // lock[0,0][0] = key[key.length -1, key.length -1][key.length - 1] + lock[0,0][0]
-  sum(background, key, lock)
-  var answer = true;
-  return answer;
+  background[key.length -1, key.length -1] = lock[0,0]
+
+  // 처음 background에 lock 세팅
+  initBackground(background, key, lock)
+
+  return sum(background, key, lock)
+  // var answer = true;
+  // return answer;
 }
-function sum(background, key, lock) {
+
+function initBackground(background, key, lock) {
   const start = key.length - 1;
   const end = lock.length + key.length - 1;
-  for(let i = start; i < end; i++ ) {
+  const flatLock = lock.flat()
+  let index = 0;
+
+  for (let i = start; i < end; i ++) {
     for (let j = start; j < end; j++) {
-      if (background[i, j] === 0) return false
-      return true
+      console.log('i, j', i, j)
+      background[i, j] = flatLock[index]
+      index++
     }
   }
 }
-solution(key, lock)
+
+// 오른쪽이동 왼쪽 이동 함수
+
+// 90도 4번 회전 함수
+
+function sum(background, key, lock) {
+  const start = key.length - 1;
+  const end = lock.length + key.length - 1;
+
+  for(let i = start; i < end; i++ ) {
+    for (let j = start; j < end; j++) {
+      if (background[i, j] === 0) {
+        // return false
+      }
+    }
+  }
+}
+console.log(solution(key, lock))
 // lock의 배열중 1이 없을 경우 리턴
 // 1. key의 M*M의 마지막 배열의 값을 Lock의 첫번째 값이랑 더한 후 lock의 모든 배열에 1이 있는지 없는지 확인
