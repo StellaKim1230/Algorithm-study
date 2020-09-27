06d259152e9338510befba2722e38d0614a456b0
Stellakim1230 <stellakim1230@gmail.com>
2020-09-27 22:05:49 +0900

    가장 먼 노드 구현중.

--- study/20200929_2.js
+++ study/20200929_2.js
@@ -1,7 +1,20 @@
 function solution(n, edge) {
-  const arr = Array.from(Array(n + 1).fill([]))
-  const queue = [[1, 0]]
-  const visit = new Set()
+  const queue = [1]
+
+  for (let i = 0; i < edge.length; i++) {
+    for (let j = 0; j < edge.length; j++) {
+      if (edge[i][0] === 1) {
+        queue.push(edge[i][1])
+      }
+
+      if (edge[0][j] === 1) {
+        queue.push(edge[i][0])
+      }
+    }
+  }
+
+  const visit = [...new Set(queue)]
+  return visit.length
 }
 
 console.log(
