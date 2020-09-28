b53a48adf9c5b301ae8f3188fe2105d293fe71b6
StellaKim1230 <stellakim1230@gmail.com>
2020-09-28 14:25:30 +0900

    풀이방법 추가.

--- study/20200929_2.js
+++ study/20200929_2.js
@@ -14,17 +14,19 @@ function solution(n, edge) {
   }
 
   const visit = [...new Set(queue)]
-  return visit.length
+  return n - visit.length
 }
 
-console.log(
-  solution(6, [
-    [3, 6],
-    [4, 3],
-    [3, 2],
-    [1, 3],
-    [1, 2],
-    [2, 4],
-    [5, 2],
-  ]),
-)
+[[1, 2], [1, 3], [2, 3], [2, 4], [2, 5], [3, 4], [3, 6]]
+
+// hashmap
+1: 2, 3
+2: 1, 3, 4, 5
+3: 1, 2, 4, 6
+4: 2, 3
+5: 2
+
+
+// queue
+queue : 2, 3, 1, 3, 4, 5, 1, 2, 4, 6, 2, 3, 2
+
